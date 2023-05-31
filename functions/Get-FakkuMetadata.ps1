5bf54bc7f7a82d334d811a215866979ded35da6a
Roi Asuncion <RoiAsuncion@gmail.com>
2023-05-31 12:29:16 -0700

    Add Safe parameter

--- functions/Get-FakkuMetadata.ps1
+++ functions/Get-FakkuMetadata.ps1
@@ -11,7 +11,10 @@ function Get-FakkuMetadata {
         [IO.DirectoryInfo]$DriverPath = (Get-Item $PSScriptRoot).Parent,
 
         [Parameter(Mandatory = $false, ParameterSetName = 'Url')]
-        [IO.DirectoryInfo]$ProfilePath = (Join-Path -Path (Get-Item $PSScriptRoot).Parent -ChildPath "profiles"),
+        [IO.DirectoryInfo]$ProfilePath = (Join-Path -Path (Get-Item $PSScriptRoot).Parent -ChildPath 'profiles'),
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'Url')]
+        [Switch]$Safe,
 
         [Parameter(Mandatory = $false, ParameterSetName = 'Url')]
         [Switch]$Headless,
@@ -22,27 +25,29 @@ function Get-FakkuMetadata {
 
     Switch ($PSCmdlet.ParameterSetName) {
         'Name' {
+            # Name search will not fallback on Selenium mostly just because it's kind of clunky
             try {
                 $FakkuUrl = Get-FakkuUrl -Name $Name
-                Write-Debug "$FakkuUrl"
+                Write-Debug "URL: $FakkuUrl"
                 $WebRequest = (Invoke-WebRequest -Uri $FakkuUrl -Method Get -Verbose:$false).Content
-                $Xml = Get-MetadataXml -WebRequest $WebRequest -URL $FakkuUrl -Provider "fakku"
+                $Xml = Get-MetadataXml -WebRequest $WebRequest -URL $FakkuUrl -Provider 'fakku'
             }
 
             # Panda name fallback
             catch {
                 try {
-                    Write-Debug "Falling back on Panda."
+                    Write-Debug 'Falling back on Panda.'
 
                     $PandaUrl = Get-PandaURL -Name $Name
-                    Write-Debug "$PandaUrl"
+                    Write-Debug "URL: $PandaUrl"
                     $WebRequest = (Invoke-WebRequest -Uri $PandaUrl -Method Get -Verbose:$false).Content
-                    $Xml = Get-MetadataXml -WebRequest $WebRequest -URL $PandaUrl -Provider "panda"
+                    $Xml = Get-MetadataXml -WebRequest $WebRequest -URL $PandaUrl -Provider 'panda'
                 } catch {
-                    Write-Warning "Work ""$Name"" not found."
+                    Write-Warning "Work `"$Name`" not found."
                     return
                 }
             }
+
             Write-Output $Xml
         }
 
@@ -52,9 +57,11 @@ function Get-FakkuMetadata {
                     'fakku.net' {
                         $Provider = 'fakku'
                     }
+
                     'panda.chaika.moe' {
                         $Provider = 'panda'
                     }
+
                     Default {
                         Write-Warning "URL `"$Url`" is not a valid FAKKU or Panda URL."
                         return
@@ -64,12 +71,13 @@ function Get-FakkuMetadata {
                 Write-Debug "UriLocation: $Provider"
                 Write-Debug "URL: $Url"
 
+                if ($Safe) { throw }
+
                 $WebRequest = (Invoke-WebRequest -Uri $Url -Method Get -Verbose:$false).Content
                 $Xml = Get-MetadataXml -WebRequest $WebRequest -URL $Url -Provider $Provider
-
             } catch {
                 try {
-                    Write-Debug "Falling back on WebDriver."
+                    Write-Debug 'Falling back on WebDriver.'
 
                     $DriverArgs = @{
                         DriverPath = $DriverPath
@@ -78,25 +86,28 @@ function Get-FakkuMetadata {
                         Incognito = $Incognito
                     }
                     $DriverObject = New-WebDriver @DriverArgs
-                    $BrowserProfile = $DriverObject.Args.Arguments[0].Split("user-data-dir=")[1]
+                    $BrowserProfile = $DriverObject.Args.Arguments[0].Split('user-data-dir=')[1]
                     $WebDriver = New-Object $DriverObject.Driver -ArgumentList $DriverObject.Args
+
                     if ($Headless -and -not (Test-Path -Path $BrowserProfile\*)) {
-                        $WebDriver.Navigate().GoToURL("https://fakku.net/login")
-                        Write-Host "Please log into FAKKU then press ENTER to continue..."
+                        $WebDriver.Navigate().GoToURL('https://fakku.net/login')
+                        Write-Host 'Please log into FAKKU then press ENTER to continue...'
                         do {
                             $KeyPressed = ([Console]::ReadKey($true))
                             Start-Sleep -Milliseconds 50
-                        } while ($KeyPressed.Key -ne "enter")
+                        } while ($KeyPressed.Key -ne 'enter')
                     }
+
                     $WebDriver.Navigate().GoToURL($Url)
                     $WebRequest = $WebDriver.PageSource
-                    $Xml = Get-MetadataXML -WebRequest $WebRequest -Url $Url -Provider "fakku"
+                    $Xml = Get-MetadataXML -WebRequest $WebRequest -Url $Url -Provider 'fakku'
                 }
                 catch {
                     Write-Warning "Error occurred while scraping `"$Url`": $PSItem"
                 }
             }
-            if ($WebDriver) {$WebDriver.Quit()}
+
+            if ($WebDriver) { $WebDriver.Quit() }
             Write-Output $Xml
         }
     }
--- functions/Set-FakkuMetadata.ps1
+++ functions/Set-FakkuMetadata.ps1
@@ -26,7 +26,10 @@ function Set-FakkuMetadata {
         [IO.DirectoryInfo]$DriverPath = (Get-Item $PSScriptRoot).Parent,
 
         [Parameter(Mandatory = $false)]
-        [IO.DirectoryInfo]$ProfilePath = (Join-Path -Path (Get-Item $PSScriptRoot).Parent -ChildPath "profiles"),
+        [IO.DirectoryInfo]$ProfilePath = (Join-Path -Path (Get-Item $PSScriptRoot).Parent -ChildPath 'profiles'),
+
+        [Parameter(Mandatory = $false)]
+        [Switch]$Safe,
 
         [Parameter(Mandatory = $false)]
         [Switch]$Headless,
@@ -38,7 +41,7 @@ function Set-FakkuMetadata {
         [Switch]$Log,
 
         [Parameter(Mandatory = $false)]
-        [IO.FileInfo]$LogPath = (Join-Path -Path (Get-Item $PSScriptRoot).Parent - ChildPath "fakku_library.log")
+        [IO.FileInfo]$LogPath = (Join-Path -Path (Get-Item $PSScriptRoot).Parent - ChildPath 'fakku_library.log')
     )
 
     function Write-FakkuLog {
@@ -71,12 +74,11 @@ function Set-FakkuMetadata {
             # URL validation
             if ($PSBoundParameters.ContainsKey('Url')) {
                 if (Test-Path -Path $FilePath -PathType Container) {
-                    Write-Warning "URL parameter can only be used with an archive, not a directory."
+                    Write-Warning 'URL parameter can only be used with an archive, not a directory.'
                     return
                 }
-
                 # A little more flexible if future providers added
-                elseif (-not ($String | Select-String "fakku.net", "panda.chaika.moe")) {
+                elseif (-not ($Url | Select-String 'fakku.net', 'panda.chaika.moe')) {
                     Write-Warning "URL `"$Url`" is not a valid FAKKU or Panda URL."
                     return
                 }
@@ -92,25 +94,26 @@ function Set-FakkuMetadata {
 
     if ($PSBoundParameters.ContainsKey('UrlFile')) {
         if ($Url) {
-            Write-Warning "URL parameter is not compatible with batch tagging."
+            Write-Warning 'URL parameter is not compatible with batch tagging.'
             return
         }
 
         # URL validation
         [array]$Links = Get-Content -Path $UrlFile |
-            ForEach-Object {$_.Trim()} |
-            Where-Object { ($_ | Select-String "fakku.net", "panda.chaika") }
+            ForEach-Object { $_.Trim() } |
+            Where-Object { ($_ | Select-String 'fakku.net', 'panda.chaika.moe') }
         if ($Links.Count -ne $Archive.Count) {
-            Write-Warning "File count does not match URL count."
+            Write-Warning 'File count does not match URL count.'
             return
         }
     }
 
+    # Main loop
     foreach ($File in $Archive) {
-        $Index = $Archive.IndexOf($File)
-        $TotalIndex = $Archive.Count
         # Re-initializes variables for next loop
         $NewUrl = $UriLocation = $Xml = $null
+        $Index = $Archive.IndexOf($File)
+        $TotalIndex = $Archive.Count
         $WorkName = $File.BaseName
         $XmlPath = Join-Path -Path $File.DirectoryName -ChildPath 'ComicInfo.xml'
 
@@ -128,10 +131,12 @@ function Set-FakkuMetadata {
                 $UriLocation = 'fakku'
                 $NewUrl = $Link
             }
+
             'panda.chaika.moe' {
                 $UriLocation = 'panda'
                 $NewUrl = $Link
             }
+
             Default {
                 $UriLocation = 'fakku'
                 $NewUrl = Get-FakkuUrl -Name $WorkName
@@ -144,6 +149,13 @@ function Set-FakkuMetadata {
 
         # Attempt with Invoke-WebRequest and match URL
         try {
+            <# NOTE:
+            There's a "bug" that will erroneously label chapter number if volumes in a series are publicly obscured.
+            The only way I can think of to avoid this is to force requesting through Selenium if a series is found.
+            I've decided to not throw an error by default and implement the parameter "Safe" instead which will force
+            the use of Selenium. #>
+            if ($Safe) { throw }
+
             $WebRequest = (Invoke-WebRequest -Uri $NewUrl -Method Get -Verbose:$false).Content
             $Xml = Get-MetadataXML -WebRequest $WebRequest -Url $NewUrl -Provider $UriLocation
             Set-MetadataXML -FilePath $File.FullName -XmlPath $XmlPath -Content $Xml
@@ -152,30 +164,33 @@ function Set-FakkuMetadata {
         # WebDriver fallback
         catch {
             try {
-                Write-Debug "Falling back on WebDriver."
+                Write-Debug 'Falling back on WebDriver...'
 
-                $DriverArgs = @{
-                    DriverPath = $DriverPath
-                    ProfilePath = $ProfilePath
-                    Headless = $Headless
-                    Incognito = $Incognito
-                }
-                $DriverObject = New-WebDriver @DriverArgs
-                $BrowserProfile = $DriverObject.Args.Arguments[0].Split("user-data-dir=")[1]
+                # Initialize new driver instance if can't find one
                 if (-not $DriverObject.Args.IsRunning) {
+                    Write-Debug 'Creating new driver instance.'
+
+                    $DriverArgs = @{
+                        DriverPath = $DriverPath
+                        ProfilePath = $ProfilePath
+                        Headless = $Headless
+                        Incognito = $Incognito
+                    }
+                    $DriverObject = New-WebDriver @DriverArgs
+                    $BrowserProfile = $DriverObject.Args.Arguments[0].Split('user-data-dir=')[1]
                     $WebDriver = New-Object $DriverObject.Driver -ArgumentList $DriverObject.Args
+
                     # Skips login process if in headless mode or browser profile is found
                     if ($Headless -and -not (Test-Path -Path $BrowserProfile\*)) {
-                        $WebDriver.Navigate().GoToURL("https://fakku.net/login")
-                        Write-Host "Please log into FAKKU then press ENTER to continue..."
-                        # This waits for any key rather than a specific key
-                        # [void]$Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
+                        $WebDriver.Navigate().GoToURL('https://fakku.net/login')
+                        Write-Host 'Please log into FAKKU then press ENTER to continue...'
                         do {
                             $KeyPressed = ([Console]::ReadKey($true))
                             Start-Sleep -Milliseconds 50
-                        } while ($KeyPressed.Key -ne "enter")
+                        } while ($KeyPressed.Key -ne 'enter')
                     }
                 }
+
                 $WebDriver.Navigate().GoToURL($NewUrl)
                 $WebRequest = $WebDriver.PageSource
                 $Xml = Get-MetadataXML -WebRequest $WebRequest -Url $NewUrl -Provider $UriLocation
@@ -185,15 +200,15 @@ function Set-FakkuMetadata {
             # Panda URL from name fallback
             catch {
                 try {
-                    Write-Debug "Falling back on Panda."
+                    Write-Debug 'Falling back on Panda...'
 
                     $UriLocation = 'panda'
                     $NewUrl = Get-PandaURL -Name $WorkName
                     Write-Debug "URL: $NewUrl"
+
                     $WebRequest = (Invoke-WebRequest -Uri $NewUrl -Method Get -Verbose:$false).Content
                     $Xml = Get-MetadataXML -WebRequest $WebRequest -Url $NewUrl -Provider $UriLocation
                     Set-MetadataXML -FilePath $File.FullName -XmlPath $XmlPath -Content $Xml
-
                 } catch {
                     Write-Warning "Error occurred while scraping `"$NewUrl`": $PSItem"
                 }
@@ -202,36 +217,32 @@ function Set-FakkuMetadata {
 
         Write-FakkuLog -Log:$Log -LogPath $LogPath -Source $UriLocation
         Write-Verbose "Set $FilePath with $NewUrl."
-        Write-Debug "Set $File using $UriLocation."
+        Write-Debug "Set using $UriLocation."
 
-        # Move archive to "./Artist/Series/Archive.ext".
+        # Move archive to "Destination/Artist/Archive.ext".
         if ($Destination) {
             try {
-                Write-Debug "Moving archive."
-                $WebRequest = $WebRequest -replace "`n|`r", ""
-                # Remove reserved path characters
+                Write-Debug 'Moving archive...'
+
+                # Remove reserved characters
                 $Title = (Get-FakkuTitle -WebRequest $WebRequest)`
-                    -replace "\\|\/|\||:|\*|\?|\`"|<|>", ""
-                $Series = (Get-FakkuSeries -WebRequest $WebRequest)`
-                    -replace "\\|\/|\||:|\*|\?|\`"|<|>", ""
-                $Artist = (Get-FakkuArtist -WebRequest $WebRequest).Split(",")[0]
-                if ($Series) {
-                    $SeriesPath = Join-Path -Path $Destination -ChildPath $Artist -AdditionalChildPath $Series
-                } else {
-                    $SeriesPath = Join-Path -Path $Destination -ChildPath $Artist -AdditionalChildPath $Title
+                    -replace '\\|\/|\||:|\*|\?|"|<|>', ''
+                $Artist = (Get-FakkuArtist -WebRequest $WebRequest).Split(',')[0]
+                $ArtistPath = Join-Path -Path $Destination -ChildPath $Artist
+
+                if (-not (Test-Path $ArtistPath)){
+                    [void](New-Item -Path $ArtistPath -ItemType 'Directory')
                 }
-                if (-not (Test-Path $SeriesPath)){
-                    [void](New-Item -Path $SeriesPath -ItemType "Directory")
-                }
-                $TargetPath = Join-Path -Path $SeriesPath -ChildPath "$Title$($File.Extension)"
+
+                $TargetPath = Join-Path -Path $ArtistPath -ChildPath "$Title$($File.Extension)"
                 Write-Debug "Target path: $TargetPath"
-                Move-Item -Path $File.FullName -Destination $TargetPath
-                $Series = $null
+                Move-Item -LiteralPath $File.FullName -Destination $TargetPath
             } catch {
                 Write-Warning "Error occurred while moving `"$File`": $PSItem"
             }
         }
     }
-    if ($WebDriver) {$WebDriver.Quit()}
-    Write-Host "Complete."
+
+    if ($WebDriver) { $WebDriver.Quit() }
+    Write-Host 'Complete.'
 }
