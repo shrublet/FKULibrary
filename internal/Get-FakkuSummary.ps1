e5dc8c1c29b612355c26e761987ba7b4c0d4e5ec
Roi Asuncion <RoiAsuncion@gmail.com>
2023-05-31 18:07:10 -0700

    Fix quotation bug

--- internal/Get-FakkuSummary.ps1
+++ internal/Get-FakkuSummary.ps1
@@ -5,9 +5,10 @@ function Get-FakkuSummary {
         [String]$WebRequest
     )
 
-    $Summary = ((($WebRequest -split '(?s)description" content="(.*?)">')[1]`
+    $Summary = ((($WebRequest -split '(?s)"og:description" content="(.*?)">')[1]`
         -split '(?s)Paperback ships|Chapters will|Downloads will')[0]`
         -split '(?s)This content is no longer available for purchase.')?.Trim()
+    $Summary = [Net.WebUtility]::HtmlDecode($Summary)
 
-    Write-Output ([Net.WebUtility]::HtmlDecode($Summary))
+    Write-Output $Summary
 }
